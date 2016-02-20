a74259cba3bd85fc643428bb1cd25ecf01213bbf
Evan Sosenko <razorx@evansosenko.com>
2016-02-19 19:34:58 -0800

    Add php index and info pages

---
+++ app/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Test PHP Application</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/3.2.0/sanitize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <main>
+      <h1>This is HTML5 Boilerplate running PHP version: <?php phpversion(); ?></h1>
+      <p>
+        <img src="img/HTML5_logo_and_wordmark.svg" width="100"
+             alt="HTML5 logo and watermark">
+        <a rel="nofollow" class="external text" href="http://www.w3.org/html/logo/index.html">W3C</a> [<a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>], <a href="https://commons.wikimedia.org/wiki/File%3AHTML5_logo_and_wordmark.svg">via Wikimedia Commons</a>
+      </p>
+    </main>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
